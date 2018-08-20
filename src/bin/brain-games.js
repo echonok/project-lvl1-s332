59089aa6d0a93c31be0844794e7a6035448e6ebd
Nick Elovsky <echonok@gmail.com>
2018-08-20 15:40:41 +0300

    3 part

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,10 +1,9 @@
-#! node
+#!/usr/bin/env node
 //import index from '..';
 console.log("Welcome to the Brain Games!");
 //console.log(index);
 console.log("Goodbye!1");
 console.log("Goodbye!2");
-
 
 //const userName = readlineSync.question('May I have your name? ');
 //console.log('Hello, ' + userName + '!');
