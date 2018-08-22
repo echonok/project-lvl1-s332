d94f7fdd8791f8e154743153e5c96d709461c745
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-08-22 20:22:05 +0300

    edit gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,21 +5,22 @@ const mainRule = 'Find the greatest common divisor of given numbers.';
 const min = 1;
 const max = 100;
 
-const commonDivisor = (numMin, numMax, firstMin) => {
-  if (numMax % numMin === 0 && firstMin % numMin === 0) {
-    return String(numMin);
-  }
-  return commonDivisor(numMin - 1, numMax, firstMin);
+const commonDivisor = (num1, num2) => {
+	const numMin = Math.min(num1, num2);
+	const numMax = Math.max(num1, num2);
+	const findCommonDivisor = (numMin, numMax, firstMin) => {
+		if (numMax % numMin === 0 && firstMin % numMin === 0) {
+			return String(numMin);
+  		}
+		return commonDivisor(numMin - 1, numMax, firstMin);
+	}
+	return findCommonDivisor(numMin, numMax, numMin)
 };
 
 const genGameData = () => {
   const num1 = getRandomNumber(min, max);
   const num2 = getRandomNumber(min, max);
-  const numMin = Math.min(num1, num2);
-  const numMax = Math.max(num1, num2);
-
-  const correctAnswer = commonDivisor(numMin, numMax, numMin);
-
+  const correctAnswer = commonDivisor(num1, num2);
   const question = `${num1} ${num2}`;
   return [question, correctAnswer];
 };
