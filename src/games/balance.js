087cfa841172c4a91c86d5d42376f2efead0a06d
Nikolay Elovsky <echonok@gmail.com>
2018-08-22 21:58:46 +0300

    ~

--- src/games/balance.js
+++ src/games/balance.js
@@ -2,41 +2,28 @@ import startNewGame from '..';
 import getRandomNumber from '../tools';
 
 const mainRule = 'Balance the given number.';
-
 const balancer = (listOfNumber) => {
-
   const arrOfNum = listOfNumber.split('').map(element => Number(element));
   console.log(arrOfNum);
-
   while (Math.max(...arrOfNum) - Math.min(...arrOfNum) > 1) {
-
     const minElem = Math.min(...arrOfNum);
     const maxElem = Math.max(...arrOfNum);
-
     const indexOfMin = arrOfNum.findIndex(elem => elem === minElem);
     const indexOfMax = arrOfNum.findIndex(elem => elem === maxElem);
-
     arrOfNum.splice(indexOfMin, 1);
     arrOfNum.splice(indexOfMax-1, 1);
-
     arrOfNum.push(minElem + 1);
     arrOfNum.push(maxElem - 1);
-
   }
-
   return arrOfNum.sort().join('');
-
 };
 
 const genGameData = () => {
-  
   let listOfNumber = '';
-
   const numberOfNumber = 5;
   for (i = 0; i < numberOfNumber; i++) { 
     listOfNumber += String(getRandomNumber(0, 9));
   }
-
   const question = someString;
   const correctAnswer = balancer(listOfNumber);
   return [question, correctAnswer];
