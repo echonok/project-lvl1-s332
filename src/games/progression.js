21e4dff27472c8f6e6130e5b889382306ba88ce0
Nick Elovsky <echonok@gmail.com>
2018-08-23 10:16:04 +0300

    ~

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,26 +3,23 @@ import getRandomNumber from '../tools';
 
 const mainRule = 'What number is missing in this progression?';
 const progressionGenerator = (sizeProgr = 10, startProgr = 4, stepProgr = 2) => {
-  let fullProgr = [];
+  const fullProgr = [];
   let i = 0;
   while (sizeProgr > i) {
     fullProgr.push(startProgr + stepProgr * i);
     i += 1;
   }
   return fullProgr;
-}
+};
 
 const genGameData = () => {
-  
-  let fullProgression = (progressionGenerator(numberOfNumber, startPosition, stepPosition));
   const numberOfNumber = 10;
   const startPosition = getRandomNumber(10, 100);
   const stepPosition = getRandomNumber(1, 9);
-
+  const fullProgression = (progressionGenerator(numberOfNumber, startPosition, stepPosition));
   const indexHiddenNumber = getRandomNumber(1, 9);
   const correctAnswer = String(fullProgression[indexHiddenNumber]);
   fullProgression[indexHiddenNumber] = '..';
-
   const question = fullProgression.join(' ');
   return [question, correctAnswer];
 };
