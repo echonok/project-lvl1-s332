bbe1bd729ada45c5a6ac9ee6f99d0c456d880c60
Nikolay Elovsky <echonok@gmail.com>
2018-08-24 20:52:01 +0300

    ~

--- src/games/mainMenu.js
+++ src/games/mainMenu.js
@@ -7,39 +7,46 @@ import startGameOfProgression from './progression';
 import startGameOfPrime from './prime';
 
 const chooseOfGame = () => {
+  console.log('Choose your game:');
   console.log('1. Game of even');
   console.log('2. Game of calc');
   console.log('3. Game of GCD');
   console.log('4. Game of balance');
   console.log('5. Game of progression');
   console.log('6. Game of prime');
-  console.log('exti');
+  console.log('0. exti');
   const chosenGame = readlineSync.question('Game:');
   return chosenGame;
 };
 
 const mainMenu = () => {
-  console.log('Welcome to the Brain Games! Choose your game:');
+  console.log('Welcome to the Brain Games!');
   switch (chooseOfGame()) {
     case '1':
       startGameOfEven();
+      chooseOfGame();
       break;
     case '2':
       startGameOfCalc();
+      chooseOfGame();
       break;
     case '3':
       startGameOfGCD();
+      chooseOfGame();
       break;
     case '4':
       startGameOfBalance();
+      chooseOfGame();
       break;
     case '5':
       startGameOfProgression();
+      chooseOfGame();
       break;
     case '6':
       startGameOfPrime();
+      chooseOfGame();
       break;
-    case 'exit':
+    case '0':
       console.log('Good bye!');
       break;
     default:
