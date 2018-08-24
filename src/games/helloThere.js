88a4415c65e5654c4bf6f57056baf3c57f6685d2
Nick Elovsky <echonok@gmail.com>
2018-08-24 15:30:50 +0300

    edit menu

--- src/games/helloThere.js
+++ src/games/helloThere.js
@@ -6,18 +6,16 @@ import startGameOfBalance from './balance';
 import startGameOfProgression from './progression';
 import startGameOfPrime from './prime';
 
-
-
 const helloThere = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`So, hi ${userName} :). Choose your game:`);
-  console.log(`1. Game of even:`);
-  console.log(`2. Game of calc:`);
-  console.log(`3. Game of GCD:`);
-  console.log(`4. Game of balance:`);
-  console.log(`5. Game of progression:`);
-  console.log(`6. Game of prime:`);
+  console.log('1. Game of even:');
+  console.log('2. Game of calc:');
+  console.log('3. Game of GCD:');
+  console.log('4. Game of balance:');
+  console.log('5. Game of progression:');
+  console.log('6. Game of prime:');
   const chosenGame = readlineSync.question('Game: ');
   switch (chosenGame) {
     case '1':
