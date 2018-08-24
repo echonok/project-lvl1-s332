a9d8edbfabbd3caad6dfb1786c0f140561080b12
Nick Elovsky <echonok@gmail.com>
2018-08-24 15:25:09 +0300

    some mistaces

--- src/games/helloThere.js
+++ src/games/helloThere.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
-import startNewGame from '..';
-import startGameOfEven from '../games/even';
+import startGameOfEven from 'even';
 import startGameOfCalc from '../games/calc';
 import startGameOfGCD from '../games/gcd';
 import startGameOfBalance from '../games/balance';
@@ -13,12 +12,12 @@ const helloThere = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`So, hi ${userName} :). Choose your game:`);
-  console.log(`			1. Game of even:`);
-  console.log(`			2. Game of calc:`);
-  console.log(`			3. Game of GCD:`);
-  console.log(`			4. Game of balance:`);
-  console.log(`			5. Game of progression:`);
-  console.log(`			6. Game of prime:`);
+  console.log(`1. Game of even:`);
+  console.log(`2. Game of calc:`);
+  console.log(`3. Game of GCD:`);
+  console.log(`4. Game of balance:`);
+  console.log(`5. Game of progression:`);
+  console.log(`6. Game of prime:`);
   const chosenGame = readlineSync.question('Game: ');
   switch (chosenGame) {
     case '1':
